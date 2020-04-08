3661e2e93878a83ab5f650216185335a3f0babb5
Gijs Koedam <g.koedam@geonovum.nl>
2020-04-08 16:37:14 +0200

    Snapshot Begrippenlijst STOP-TPOD v.098.2-kern

--- Begrippenlijst/config.js
+++ Begrippenlijst/config.js
@@ -59,7 +59,7 @@ var respecConfig =
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   //-- de github repository waar je documentn in staat --------------------------------
-  github: "https://github.com/geonovum/TPOD/Begrippenlijst",
+  github: "https://github.com/geonovum/TPOD/",
   editors: 
  [
   {
